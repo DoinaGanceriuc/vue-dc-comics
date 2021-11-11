af9975e20410aecf130d107b46d51ef9381e1bba
DoinaGanceriuc <doinaganceriuc@gmail.com>
2021-11-11 14:50:50 +0100

    complete details siteheader component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,34 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+      <SiteHeader/>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import SiteHeader from './components/SiteHeader.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+  // HelloWorld
+    SiteHeader
   }
 }
 </script>
 
 <style lang="scss">
-#app {
+/* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+} */
+@import './assets/scss/variables.scss';
+@import './assets/scss/common.scss';
+
 </style>
---
+++ src/components/SiteHeader.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="">
+    <header id="site_header">
+      <div class="logo">
+        <img src="../assets/dc-logo.png" alt="" width="60">
+      </div>
+      <ul>
+        <li>CHARACTERS</li>
+        <li class="list_active">COMICS</li>
+        <li>MOVIES</li>
+        <li>TV</li>
+        <li>GAMES</li>
+        <li>COLLECTIBLES</li>
+        <li>VIDEOS</li>
+        <li>FANS</li>
+        <li>NEWS</li>
+        <li>SHOP</li>
+      </ul>
+    </header>
+
+  </div>
+</template>
+
+<script>
+</script>
+
+<style lang="scss">
+@import '../assets/scss/variables.scss';
+#site_header{
+  display: flex;
+  justify-content: space-between;
+  align-items: end;
+  padding-top: 2rem;
+.logo {
+  padding-bottom: 1rem;
+}
+ul {
+  display: flex;
+  list-style: none;
+  li {
+    padding: 0 1rem;
+  }
+  .list_active {
+      color: $color_primary;
+      &::after {
+        content: "";
+        display: block;
+        width: 50px;
+        height: 5px;
+        background-color: $color_primary;
+        margin: auto;
+        margin-top: 2rem;
+      }
+    }
+
+}
+}
+</style>
