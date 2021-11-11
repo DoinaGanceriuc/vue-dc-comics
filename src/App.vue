003c4777f270179c45c2f4a6dabf692a7af2e7a1
DoinaGanceriuc <doinaganceriuc@gmail.com>
2021-11-11 17:46:40 +0100

    comlete details sitefooter component

--- src/App.vue
+++ src/App.vue
@@ -2,31 +2,26 @@
   <div id="app">
       <SiteHeader/>
       <SiteMain/>
+      <SiteFooter/>
   </div>
 </template>
 
 <script>
 import SiteHeader from './components/SiteHeader.vue'
 import SiteMain from './components/SiteMain.vue'
+import SiteFooter from './components/SiteFooter.vue'
 
 export default {
   name: 'App',
   components: {
     SiteHeader,
-    SiteMain
+    SiteMain,
+    SiteFooter
   }
 }
 </script>
 
 <style lang="scss">
-/* #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-} */
 @import './assets/scss/variables.scss';
 @import './assets/scss/common.scss';
 
---
+++ src/components/SiteFooter.vue
@@ -0,0 +1,152 @@
+<template>
+  <div>
+    <footer id="site_footer">
+       <div class="informations container">
+         <div class="row">
+          <div class="col-4">
+            <h3>DC COMICS</h3>
+          <ul>
+            <li><a href="#">Characters</a></li>
+            <li><a href="#">Comics</a></li>
+            <li><a href="#">Movies</a></li>
+            <li><a href="#">TV</a></li>
+            <li><a href="#">Games</a></li>
+            <li><a href="#">Videos</a></li>
+            <li><a href="#">News</a></li>
+          </ul>
+          <h3 class="list_shop">SHOP</h3>
+          <ul>
+            <li><a href="#">Shop DC</a></li>
+            <li><a href="#">Shop DC Collectibles</a></li>
+          </ul>
+         </div>
+         <div class="col-4">
+            <h3>DC</h3>
+          <ul>
+            <li><a href="#">Terms Of Use</a></li>
+            <li><a href="#">Privacy Policy (New)</a></li>
+            <li><a href="#">Ad Choices</a></li>
+            <li><a href="#">Advertising</a></li>
+            <li><a href="#">Jobs</a></li>
+            <li><a href="#">Subscriptions</a></li>
+            <li><a href="#">Talent Workshops</a></li>
+            <li><a href="#">CPSC Certificates</a></li>
+            <li><a href="#">Ratings</a></li>
+            <li><a href="#">Shop Help</a></li>
+            <li><a href="#">Contact Us</a></li>
+          </ul>
+         </div >
+         <div class="col-4">
+            <h3>SITES</h3>
+          <ul>
+            <li><a href="#">DC</a></li>
+            <li><a href="#">MAD Magazine</a></li>
+            <li><a href="#">DC Kids</a></li>
+            <li><a href="#">DC Universe</a></li>
+            <li><a href="#">DC Power Visa</a></li>
+          </ul>
+         </div>
+         </div>
+         <!-- /.row -->
+         <div class="footer_logo">
+         </div>
+       </div>
+      <div class="socials">
+        <div class="container">
+          <div class="btn">
+            <a href="">SIGN-UP NOW!</a>
+          </div>
+          <div class="left_icons">
+            <h4>FOLLOW US</h4>
+            <img src="../assets/footer-facebook.png" alt="">
+            <img src="../assets/footer-twitter.png" alt="">
+            <img src="../assets/footer-youtube.png" alt="">
+            <img src="../assets/footer-pinterest.png" alt="">
+            <img src="../assets/footer-periscope.png" alt="">
+          </div>
+        </div>
+        <!-- /.container -->
+      </div>
+    </footer>
+ <!-- /#site_footer -->
+  </div>
+</template>
+
+<script>
+</script>
+
+<style lang="scss">
+@import '../assets/scss/variables.scss';
+#site_footer{
+  background-image: url("../assets/footer-bg.jpg");
+  background-repeat: no-repeat;
+  background-position-x: center;
+  background-size: cover;
+.informations {
+  display: flex;
+  justify-content: space-between;
+.row {
+ display: flex;
+ padding: 2rem 0;
+ width: 40%;
+ .col-4 {
+   width: calc(100% / 3);
+
+ }
+h3{
+  color: $color_text_primary_main;
+  padding-bottom: 0.5rem;
+}
+.list_shop {
+  padding-top: 1rem;
+}
+ul {
+  list-style: none;
+  & a {
+    color: #959577;
+    font-size: 0.7rem;
+     text-decoration: none;
+  }
+}
+
+}
+.footer_logo {
+  background-image: url("../assets/dc-logo-bg.png");
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+  height: 350px;
+  width: 500px;
+}
+}
+.socials {
+  background-color: #303030;
+  padding: 2rem 0;
+  & .container {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+.btn {
+  border: 1px solid $color_primary;
+  padding: 1rem;
+
+  & a {
+    text-decoration: none;
+    color: $color_text_primary_main;
+  }
+}
+.left_icons {
+  display: flex;
+  align-items: center;
+
+  & h4 {
+  color: $color_primary;
+}
+  & img {
+    padding: 0 0.5rem;
+  }
+}
+}
+}
+}
+</style>
