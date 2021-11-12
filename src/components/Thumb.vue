b719d608b49e8bf07d0531590b419156b1f8a3fc
DoinaGanceriuc <doinaganceriuc@gmail.com>
2021-11-12 11:35:56 +0100

    add thumb component

---
+++ src/components/Thumb.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="thumb col-2">
+      <img :src="albumImage" alt="">
+      <h5>{{typeAlbum}}</h5>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    albumImage: String,
+    typeAlbum: String
+  }
+}
+</script>
+
+<style>
+</style>
